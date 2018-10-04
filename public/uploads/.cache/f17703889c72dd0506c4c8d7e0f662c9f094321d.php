c60525042ccd81df06cb967ece6751a5ab6cdfdc
Bryan Baird <bryan@kerigan.com>
2018-10-04 13:14:02 +0000

    local changes

---
+++ public/uploads/.cache/f17703889c72dd0506c4c8d7e0f662c9f094321d.php
@@ -0,0 +1,34 @@
+<footer class="sticky-footer bg-light py-3 sizable">
+    <div class="container pt-4 text-center text-md-left">
+        <div class="row align-items-start">
+            <div class="mb-4 col-md-6 col-lg-12 col-xl-auto text-center">
+                <img src="/themes/wordplate/assets/images/seal-footer.png" class="img-fluid" alt="Gulf county Clerk of Courts and Comptroller" aria-hidden="true" >
+            </div>
+            <div class="mb-4 col-md-6 col-lg-auto text-secondary">
+                <p class="text-uppercase mb-2"><strong>Custodian of Public Records</strong></p>
+                <?php echo get_field('mailing_address','option'); ?>
+
+            </div>
+            <div class="mb-4 col-md-6 col-lg-auto text-secondary">
+                <p class="text-uppercase mb-2"><strong>Port St. Joe</strong></p>
+                <?php echo get_field('psj_location','option'); ?>
+
+            </div>
+            <div class="mb-4 col-md-6 col-lg-auto text-secondary">
+                <p class="text-uppercase mb-2"><strong>Wewahitchka</strong></p>
+                <?php echo get_field('wewa_location','option'); ?>
+
+            </div>
+        </div>
+    </div>
+    <hr class="border-white">
+    <p class="copyright text-center text-secondary text-uppercase">&copy;<?php echo e(date('Y')); ?> <?php echo e(get_bloginfo()); ?>. &nbsp; 
+        <a class="text-underline d-block d-md-inline text-secondary text-uppercase" href="/privacy-policy/" >Privacy&nbsp;Policy</a> 
+        <span class="d-none d-md-inline ">|</span>
+        <a class="text-underline d-block d-md-inline text-secondary text-uppercase" href="/accessibility-policy/" >Accessibility&nbsp;Policy</a> 
+        <span class="d-none d-md-inline">|</span>
+        <a class="text-underline d-block d-md-inline text-secondary text-uppercase" href="/contact/" >Contact</a> 
+        <span class="d-none d-md-inline">|</span>
+        <a class="text-underline d-block d-md-inline text-secondary text-uppercase" href="/sitemap_index.xml" >Site Map</a> 
+    </p>
+</footer>
