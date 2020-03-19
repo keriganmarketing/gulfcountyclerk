fa9d105c6f691d72a8031f0c06b1e826b0b3ca10
Bryan Baird <bryan@kerigan.com>
2020-03-19 10:39:47 -0400

    change content area on home page:

--- webpack.mix.js
+++ webpack.mix.js
@@ -16,9 +16,11 @@ mix.options({
 
 mix.js('resources/assets/scripts/app.js', 'scripts');
 mix.sass('resources/assets/styles/app.scss', 'styles');
+mix.sass('resources/assets/styles/admin.scss', 'styles');
+
 mix.styles([
-    'node_modules/alertifyjs/build/css/alertify.min.css',
-    'node_modules/alertifyjs/build/css/themes/bootstrap.min.css',
+    // 'node_modules/alertifyjs/build/css/alertify.min.css',
+    // 'node_modules/alertifyjs/build/css/themes/bootstrap.min.css',
     'public/themes/wordplate/assets/styles/app.css'
 ], 'public/themes/wordplate/assets/styles/main.css');
 
