a19a6cbc22b367b42ff1f57a5833aa89ff670666
Bryan Baird <bryan@kerigan.com>
2020-11-06 15:55:03 -0500

    repo cleanup

--- public/wflogs/config.php
+++
@@ -1,6 +0,0 @@
-<?php exit('Access denied'); __halt_compiler(); ?>
-******************************************************************
-This file is used by the Wordfence Web Application Firewall. Read 
-more at https://docs.wordfence.com/en/Web_Application_Firewall_FAQ
-******************************************************************
-a:6:{s:9:"wafStatus";s:7:"enabled";s:30:"learningModeGracePeriodEnabled";i:0;s:7:"authKey";s:64:"~b7EFq)2>J%hd(B~IhgR!e3!g^/j[5*rGJrm,_B8ocKco4/(s{F$T8E1havWzF_w";s:7:"version";s:5:"1.0.4";s:11:"wafDisabled";b:0;s:13:"attackDataKey";i:2387;}
